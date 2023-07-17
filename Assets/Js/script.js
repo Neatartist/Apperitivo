ea35ccb6860cc3808d2e475e7815a138a31e0915
Daniel Van Kirk <davk0810@gmail.com>
2023-07-17 11:32:15 -0400

    google maps displays

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,2 +1,19 @@
 const APIkey = "AIzaSyA9renZ8ki4q7rSWPiEqFjv8sfVjIqEnm0";
 const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=";
+
+function initialize () {
+  let mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(45.464211, 9.191383),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  let map = new google.maps.Map(document.getElementById("googlemap"), mapOptions);
+  let marker = new google.maps.Marker({
+    position: new google.maps.LatLng(45.464211, 9.191383),
+    map: map,
+    title: "Apperitivo"
+  });
+}
+
+// DISPLAY THE MAP
+google.maps.event.addDomListener(window, "load", initialize);
